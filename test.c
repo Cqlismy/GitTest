67878a7ff2796ddd57288e870944c425e6e1c8c0
Cqlismy <157823712@qq.com>
2019-11-11 20:15:31 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,7 +1,21 @@
-#include <stdio.h>
-#include <string.h>
+#include <hardware/hardware.h>
 
-int main(int argc, char *argv[])
-{
-	return 0;
-}
+typedef struct led_module {
+	struct hw_module_t common;
+	...
+	...
+} led_module_t;
+
+struct led_module HAL_MODULE_INFO_SYM = {
+	.common = {
+		.tag = HARDWARE_MODULE_TAG,
+		.module_api_version = LED_MODULE_API_VERSION_1_0,
+		.hal_api_version = HARDWARE_HAL_API_VERSION,
+		.id = LED_HARDWARE_MODULE_ID,
+		.name = "Defaule Led HAL",
+		.author = "hly",
+		.methods = &led_module_methods,
+	},
+	...
+	...
+};

4a4d50deadbb58b6d38af7e5d00b638f22f5562e
huangly <157823712@qq.com>
2020-01-16 17:18:52 +0800

    create app.c and modified files

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ Name:myGitTest
 
 About:This is a Test for GitHub.
 
-Author:HLY
+Author:huangly
+
+Time:2020/1/1
+
+Version:1.0
---
+++ app.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("This my first app!\n");
+
+	return 0;
+}
+
+

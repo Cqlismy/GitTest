788ecd49b065c9bebbfee6cb118c48b5d0865102
Cqlismy <157823712@qq.com>
2020-04-19 20:44:35 +0800

    delete

--- Makefile
+++
@@ -1,7 +0,0 @@
-# A simple Makefile for test app
-
-all:
-	gcc test.c -o test
-
-clean:
-	rm -rf test
--- app.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[])
-{
-	printf("This my first app!\n");
-
-	return 0;
-}
-
